fe648d495dbf4f3dbabcabbd668b27e42c578152
Erwan MAIRE <erwan.maire@ens.uvsq.fr>
2022-02-11 10:49:44 +0100

    test

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -6,8 +6,8 @@
 # https://github.com/uvsq22005047/projet_tas_de_sable.git
 #########################################################
 
-<<<<<<< HEAD
-#eerezhdsg
-=======
-#modifivation
->>>>>>> 5dde207e8bce716413cf017d6910805c312b66ae
+
+
+
+
+#test
