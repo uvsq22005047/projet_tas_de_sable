19ac6aa7f4f10e8ff0afbee8f23eb5c1b6e2977b
Clémence GERMETTE <clemence.germette@ens.uvsq.fr>
2022-02-11 12:13:57 +0100

    plan global du programme

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -5,5 +5,26 @@
 # Erwan MAIRE 
 # https://github.com/uvsq22005047/projet_tas_de_sable.git
 #########################################################
+import tkinter as tk
 
+#création de la fenetre grafique
+
+
+#création des variables global
+
+
+#création des fonction
+def création_configuration():
+    """fonction qui crée un configuration"""
+    pass
+
+
+
+#création des widgets
+
+
+#placement des widgets
+
+
+#lancement de la fenetre
 
