0ad8200e5b25790111ff71a898dabfcd15b83fe6
Sofia TERKI <sofia.terki@ens.uvsq.fr>
2022-03-04 19:45:06 +0100

    fonction affichage couleur et pile centrée

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -29,12 +29,30 @@ historique = []
 
 def affichage_couleur():
     canvas.delete('all')
+    del liste_widgets[:] 
+    for i in range(TAILLE_GRILLE):
+        for j in range(TAILLE_GRILLE):
+           liste_widgets.append(canvas.create_rectangle(i*(LARGEUR/TAILLE_GRILLE), j*(HAUTEUR/TAILLE_GRILLE),
+           (i+1)*(LARGEUR/TAILLE_GRILLE), (j+1)*(HAUTEUR/TAILLE_GRILLE),
+           fill = '#{:02x}{:02x}{:02x}'.format(2*configuration[i][j], 2*configuration[i][j], 2*configuration[i][j])))
+
+
+"""Fonction qui associe à chaque valeur de la configuration une couleur"""
+def affichage_couleur():
+    canvas.delete('all')
     del liste_widgets[:]
     for i in range(TAILLE_GRILLE):
         for j in range(TAILLE_GRILLE):
+            if configuration[i][j] == 0:
+                color = "red"
+            elif configuration[i][j] == 1:
+                color = "blue"
+            elif configuration[i][j] == 2:
+                color = "yellow"
+            elif configuration[i][j] == 3:
+                color = "green"
             liste_widgets.append(canvas.create_rectangle(i*(LARGEUR/TAILLE_GRILLE), j*(HAUTEUR/TAILLE_GRILLE),
-             (i+1)*(LARGEUR/TAILLE_GRILLE), (j+1)*(HAUTEUR/TAILLE_GRILLE),
-             fill = '#{:02x}{:02x}{:02x}'.format(2*configuration[i][j], 2*configuration[i][j], 2*configuration[i][j])))
+             (i+1)*(LARGEUR/TAILLE_GRILLE), (j+1)*(HAUTEUR/TAILLE_GRILLE),fill = color, outline = color))
 
 
 def affichage():
@@ -54,7 +72,7 @@ def config_aleatoire():
     if configuration != []:
         configuration = []
     for i in range(TAILLE_GRILLE):
-            configuration.append([random.randint(0, 15) for j in range(TAILLE_GRILLE)])
+            configuration.append([random.randint(0, 3) for j in range(TAILLE_GRILLE)])
     print(configuration)
     affichage_couleur()
 
@@ -66,7 +84,27 @@ def config_max():
             configuration.append([4 for j in range(TAILLE_GRILLE)])
     affichage_couleur()
 
-           
+
+"""Fonction qui attribue N grains de sable à la case du milieu et 0 aux autres"""
+def config_Pile_centree():
+    global configuration
+    N = int(input("Entrer un chiffre"))
+    for i in range(TAILLE_GRILLE):
+        for j in range(TAILLE_GRILLE):
+            configuration[i][j] = 0
+    configuration[TAILLE_GRILLE/2][TAILLE_GRILLE/2] = N
+    affichage_couleur()
+
+
+""""Fonction qui additionne la configuration Max stable avec elle-même"""
+def config_double_max_stable():
+    pass
+
+def config_Identity():
+    pass
+
+
+
 def sauvegarder_configuration():
     global historique
     historique.append(configuration)
@@ -105,7 +143,12 @@ def stabilisation():
     affichage_couleur()
     instable()  
     if liste_instable != []:
-        canvas.after(1, stabilisation)         
+        canvas.after(1, stabilisation)    
+
+"""Fonction qui permet d'additionner les éléments case par case deux configurations """
+def addition_configurations():
+    pass
+
         
 
 #création de la fenetre graphique
@@ -118,13 +161,15 @@ button_creation_configuration = tk.Button (text ='Création', command = lambda :
 button_sauvegarder = tk.Button(text = 'Sauvegarder', command = lambda : sauvegarder_configuration())
 button_stabilisation = tk.Button(text="Stabiliser", command = lambda : stabilisation()) 
 button_max = tk.Button(text="max", command = lambda : config_max()) 
+button_pile_centree = tk.Button(text = "Pile centrée", command = lambda : config_Pile_centree() )
 
 #placement des widgets
-canvas.grid(column=0, columnspan=4)
+canvas.grid(column=0, columnspan=4) 
 button_creation_configuration.grid(column=0, row = 1)
 button_sauvegarder.grid(column=2, row = 1)
 button_stabilisation.grid(column=1, row = 1)
 button_max.grid(column=3, row = 1)
+button_pile_centree(column = 4, row = 1)
 
 #lancement de la fenetre
 racine.mainloop()
